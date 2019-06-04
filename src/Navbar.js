84249cd66ce2eba564fae17289ac0abc51cdf634
Logan Miller <logan.h.miller@gmail.com>
2019-06-03 18:11:41 -0700

    changed formatting

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { green } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    border: green,
+  },
+}));
 
 function Navbar() {
-    return (
-        <AppBar positon="fixed">
-            <Toolbar>
-                <h2>Earthquake Mapper</h2>
-            </Toolbar>
-        </AppBar>
-    );
+    const classes = useStyles();
+
+  return (
+    <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+            <Typography variant="h6" noWrap>
+                Earthquake Mapper
+            </Typography>
+        </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Navbar;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+const drawerWidth = 350;
 
 const useStyles = makeStyles(theme => ({
-    sidebar: {
-        width: "7000px",
-        flexShrink: 0,
-    },
-    toolbar: theme.mixins.toolbar,
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: theme.mixins.toolbar,
 }));
 
 function Sidebar() {
+
     const classes = useStyles();
 
-    return (
-        <Drawer variant="permanent" className="sidebar">
-            <div className={classes.toolbar}/>
-            <div>   
-                <ul>
-                    <li>This</li>
-                    <li>Will</li>
-                    <li>Be</li>
-                    <li>Where</li>
-                    <li>Our</li>
-                    <li>Data</li>
-                    <li>Go</li>
-                </ul>
-            </div>
-        </Drawer>
-    );
+  return (
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.toolbar} />
+        <List>
+            <ListItem>This</ListItem>
+            <ListItem>Is</ListItem>
+            <ListItem>Where</ListItem>
+            <ListItem>Our</ListItem>
+            <ListItem>Data</ListItem>
+            <ListItem>Will</ListItem>
+            <ListItem>Go</ListItem>
+        </List>
+      </Drawer>
+  );
 }
 
 export default Sidebar;
