6e22e2356f0c815f5cee0f95713c707b4aa2752a
Michelle <michellekuo14@gmail.com>
2019-06-09 21:00:20 -0700

    add about, buggg

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,33 +2,39 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { green } from '@material-ui/core/colors';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(theme => ({
+
+const styles = theme => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     border: green,
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-}));
+});
 
-function Navbar() {
-  const classes = useStyles();
+class Navbar extends React.Component {
 
-  return (
-    <AppBar position="fixed" className={classes.appBar}>
-      <Toolbar>
-        <Typography variant="h6" noWrap>
-          Earthquake mapper
-            </Typography>
-        <Button className={classes.menuButton} color="inherit">About</Button>
-      </Toolbar>
-    </AppBar>
-  );
+  handleClick() {
+    console.log('Hi');
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Earthquake mapper
+              </Typography>
+          <Button color="inherit" onClick={this.handleClick}>About</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+
 }
-
-export default Navbar;
+export default (makeStyles(styles)(Navbar));
+//export default Navbar;
