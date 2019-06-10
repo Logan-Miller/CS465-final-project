4446d2eec00eb9be9ba47cde482d2eeedf1910d9
Logan Miller <logan.h.miller@gmail.com>
2019-06-09 17:48:55 -0700

    added support for user selection

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,13 +6,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 const axios = require('axios');
-const drawerWidth = 250;
+const drawerWidth = 300;
 
 const styles = theme => ({
   drawer: {
@@ -26,6 +25,12 @@ const styles = theme => ({
     flexGrow: 1,
     padding: theme.spacing(3),
   },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+  group: {
+    margin: theme.spacing(1, 0),
+  },
   toolbar: theme.mixins.toolbar,
 });
 
@@ -35,55 +40,100 @@ class Sidebar extends React.Component {
     super(props);
     this.state = {
       earthquakes: [],
+      selectedMagnitude: "1.0",
+      selectedTimeframe: "hour"
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleClick() {
     const self = this;
 
-    axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson")
+    axios.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/" 
+    + this.state.selectedMagnitude + "_" + this.state.selectedTimeframe + ".geojson")
       .then(function(response) {
-        console.log(response.data.features);
         self.setState({
-          earthquakes: response.data.features
+          earthquakes: response.data.features,
         })
       })
       .catch(function(err) {
         console.log(err.message);
       });
+  }
 
+  handleChange(event) {
+    if(event.target.name === "magnitude"){
+      this.setState({
+        selectedMagnitude: event.target.value
+      })  
+    }
+
+    else if(event.target.name === "timeframe") {
+      this.setState({
+        selectedTimeframe: event.target.value
+      })  
+    } 
   }
 
   render() {
 
     const { classes } = this.props;
     return (
-        <Drawer
-            className={classes.drawer}
-            variant="permanent"
-            classes={{
-            paper: classes.drawerPaper,
-            }}
-        >
-            <div className={classes.toolbar} />
-            <div>
-                <button onClick={this.handleClick}>Its a button</button>
-            </div>
-            <List>
-               {
-                 this.state.earthquakes.map((earthquake, index) => (
-                  <ListItem key = {index}> 
-                    <ListItemText
-                      primary= {"Magnitude: " + earthquake.properties.mag}
-                      secondary={"Location: " + earthquake.properties.title}
-                    />
-                  </ListItem>
-                 ))
-               }
-
-            </List>
-        </Drawer>
+      <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+          paper: classes.drawerPaper,
+          }}
+      >
+        <div className={classes.toolbar} />
+        <div>
+            <button onClick={this.handleClick}>View Earthquakes</button>
+        </div>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
+          <FormControl component="fieldset" className={classes.formControl}>
+            <FormLabel component="legend">Magnitude</FormLabel>
+            <RadioGroup
+              aria-label="Magnitude"
+              name="magnitude"
+              className={classes.group}
+              defaultValue="1.0"
+              onChange={this.handleChange}
+            >
+              <FormControlLabel value="1.0" control={<Radio />} label="1.0+" />
+              <FormControlLabel value="2.5" control={<Radio />} label="2.5+" />
+              <FormControlLabel value="4.5" control={<Radio />} label="4.5+" />
+            </RadioGroup>
+          </FormControl>
+          <FormControl component="fieldset" className={classes.formControl} >
+            <FormLabel component="legend" >Time frame</FormLabel>
+            <RadioGroup
+              aria-label="Timeframe"
+              name="timeframe"
+              className={classes.group}
+              defaultValue="hour"
+              onChange={this.handleChange}
+            >
+              <FormControlLabel value="hour" control={<Radio color="primary"/>} label="1 Hour" />
+              <FormControlLabel value="day" control={<Radio color="primary"/>} label="1 Day" />
+              <FormControlLabel value="week" control={<Radio color="primary"/>} label="1 Week" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+        <List>
+            {
+              this.state.earthquakes.map((earthquake, index) => (
+              <ListItem key = {index}> 
+                <ListItemText
+                  primary= {"Magnitude: " + earthquake.properties.mag}
+                  secondary={"Location: " + earthquake.properties.title}
+                />
+              </ListItem>
+              ))
+            }
+        </List>
+      </Drawer>
     );
   }
 }
